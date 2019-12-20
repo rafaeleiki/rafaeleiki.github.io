e868732c38ee2e3a50e8a647ff9301b51f6e9448
Rafael Eiki <rafaeleiki@yahoo.com.br>
2019-12-20 00:26:08 -0300

    Fixed animation structure

--- src/app/animations/animations.ts
+++ src/app/animations/animations.ts
@@ -1,17 +1,22 @@
 import {animate, animateChild, group, query, style, transition, trigger} from '@angular/animations';
 
-const slideTo = (direction, top: any = 0) => [
+const homeTop = '24.5%';
+const academicTop = '11.5%';
+const projectsTop = '16%';
+const professionalTop = '0';
+
+const slideLeftHome = [
   query(':leave', [
     style({
       position: 'absolute',
-      top,
+      top: homeTop,
       left: 0,
       width: '100%',
       height: '100%'
     })
   ]),
   query(':enter', [
-    style({ [direction]: '110%', position: 'relative' }),
+    style({ left: '110%', position: 'relative' }),
   ]),
   query(':leave', animateChild()),
   group([
@@ -20,26 +25,156 @@ const slideTo = (direction, top: any = 0) => [
     ]),
     query(':enter', [
       group([
-        animate('600ms ease-out', style({ [direction]: 0 })),
+        animate('600ms ease-out', style({ left: 0 })),
       ])
     ])
   ]),
   query(':enter', animateChild()),
 ];
 
-const homeTop = '24.5%';
-const academicTop = '11.5%';
-const projectsTop = '16%';
-const professionalTop = '0';
+export const slideRightAcademic = [
+  query(':leave', [
+    style({
+      position: 'absolute',
+      top: academicTop,
+      left: 0,
+      width: '100%',
+      height: '100%'
+    })
+  ]),
+  query(':enter', [
+    style({ right: '110%', position: 'relative' }),
+  ]),
+  query(':leave', animateChild()),
+  group([
+    query(':leave', [
+      animate('300ms ease-out', style({ opacity: 0 }))
+    ]),
+    query(':enter', [
+      group([
+        animate('600ms ease-out', style({ right: 0 })),
+      ])
+    ])
+  ]),
+  query(':enter', animateChild()),
+];
+
+export const slideLeftAcademic = [
+  query(':leave', [
+    style({
+      position: 'absolute',
+      top: academicTop,
+      left: 0,
+      width: '100%',
+      height: '100%'
+    })
+  ]),
+  query(':enter', [
+    style({ left: '110%', position: 'relative' }),
+  ]),
+  query(':leave', animateChild()),
+  group([
+    query(':leave', [
+      animate('300ms ease-out', style({ opacity: 0 }))
+    ]),
+    query(':enter', [
+      group([
+        animate('600ms ease-out', style({ left: 0 })),
+      ])
+    ])
+  ]),
+  query(':enter', animateChild()),
+];
+
+export const slideLeftProjects = [
+  query(':leave', [
+    style({
+      position: 'absolute',
+      top: projectsTop,
+      left: 0,
+      width: '100%',
+      height: '100%'
+    })
+  ]),
+  query(':enter', [
+    style({ left: '110%', position: 'relative' }),
+  ]),
+  query(':leave', animateChild()),
+  group([
+    query(':leave', [
+      animate('300ms ease-out', style({ opacity: 0 }))
+    ]),
+    query(':enter', [
+      group([
+        animate('600ms ease-out', style({ left: 0 })),
+      ])
+    ])
+  ]),
+  query(':enter', animateChild()),
+];
+
+export const slideRightProjects = [
+  query(':leave', [
+    style({
+      position: 'absolute',
+      top: projectsTop,
+      left: 0,
+      width: '100%',
+      height: '100%'
+    })
+  ]),
+  query(':enter', [
+    style({ right: '110%', position: 'relative' }),
+  ]),
+  query(':leave', animateChild()),
+  group([
+    query(':leave', [
+      animate('300ms ease-out', style({ opacity: 0 }))
+    ]),
+    query(':enter', [
+      group([
+        animate('600ms ease-out', style({ right: 0 })),
+      ])
+    ])
+  ]),
+  query(':enter', animateChild()),
+];
+
+export const slideRightProfessional = [
+  query(':leave', [
+    style({
+      position: 'absolute',
+      top: professionalTop,
+      left: 0,
+      width: '100%',
+      height: '100%'
+    })
+  ]),
+  query(':enter', [
+    style({ right: '110%', position: 'relative' }),
+  ]),
+  query(':leave', animateChild()),
+  group([
+    query(':leave', [
+      animate('300ms ease-out', style({ opacity: 0 }))
+    ]),
+    query(':enter', [
+      group([
+        animate('600ms ease-out', style({ right: 0 })),
+      ])
+    ])
+  ]),
+  query(':enter', animateChild()),
+];
 
 export const slideInOutAnimation =
   trigger('routeAnimations', [
-    transition('Home => *', slideTo('left', homeTop)),
-    transition('Academic => Home', slideTo('right', academicTop)),
-    transition('Academic => Projects', slideTo('left', academicTop)),
-    transition('Academic => Projects', slideTo('left', academicTop)),
-    transition('Projects => Home', slideTo('right', projectsTop)),
-    transition('Projects => Academic', slideTo('right', projectsTop)),
-    transition('Projects => Professional', slideTo('left', projectsTop)),
-    transition('Professional => *', slideTo('right', projectsTop)),
+    transition('Home => *', slideLeftHome),
+    transition('Academic => Home', slideRightAcademic),
+    transition('Academic => Projects', slideLeftAcademic),
+    transition('Academic => Projects', slideLeftAcademic),
+    transition('Projects => Home', slideRightProjects),
+    transition('Projects => Academic', slideRightProjects),
+    transition('Projects => Professional', slideLeftProjects),
+    transition('Professional => *', slideRightProfessional),
   ]);
