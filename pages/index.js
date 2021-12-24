098e7497c0a2518ba2e385fbee695c6febb52f9c
Rafael Eiki <rafaeleikimi@gmail.com>
2021-12-24 01:20:02 -0300

    Added animations to the home page

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home(props) {
   const f = (id) => formatMessage({ id });
 
   return (
-    <div className="container is-full">
+    <div className="">
       <Head>
         <title>Rafael Eiki</title>
         <link rel="icon" href="/favicon.ico" />
@@ -21,32 +21,37 @@ export default function Home(props) {
 
       <Navbar />
 
-      <main className="columns home-hero">
-        <div className="column">
+      <main className="columns is-mobile home-hero">
+        <div className="column cover-column">
           <Image 
             src="/images/rafael-full.webp"
-            layout="responsive"
+            layout="fill"
+            objectFit="cover"
             width={1000}
             height={1500}
-            className="cover-image"
           />
         </div>
 
         <div className="column p-6 title-column">
-          <h1 className="is-size-1 is-hidden-mobile">
+          <h1 className="is-size-1 block">
             Rafael Eiki
           </h1>
 
-          <p className="is-size-4">Desenvolvedor Full-Stack,</p>
-          <p className="is-size-4">Pesquisador de EdTech,</p>
-          <p className="is-size-4">Empreendedor</p>
+          <p className="is-size-4 block">Desenvolvedor Full-Stack</p>
+          <p className="is-size-4 block">Pesquisador de EdTech</p>
+          <p className="is-size-4 block">Empreendedor</p>
+
+          <div className="column-footer">
+            sadsad
+            <style jsx>{`
+              .column-footer {
+                position: absolute;
+                bottom: 0;
+              }
+            `}</style>
+          </div>
         </div>
       </main>
-
-      <footer className="footer">
-        <p>Rafael Eiki - 2021</p>
-        <p>Bacharel em Ciência da Computação pela UNICAMP</p>
-      </footer>
 
     </div>
   )
