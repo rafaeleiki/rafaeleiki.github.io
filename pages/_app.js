c9936ae3143f0609f858db80b13c0533dc1915f9
Rafael Eiki <rafaeleikimi@gmail.com>
2022-01-09 01:19:35 -0300

    Added Google Tag Manager

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
+import { useEffect } from "react";
 import Head from 'next/head';
+import { useRouter } from "next/router";
 
 import Navbar from '../components/navbar';
 
@@ -11,6 +13,18 @@ import '../styles/home.css';
 import '../styles/researcher.css';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      if (isProduction) gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <Head>
--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,22 @@ class MyDocument extends Document {
   render() {
     return (
       <Html >
-        <Head />
+        <Head>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-Z9LJLZLN67"></script>
+          <script
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-Z9LJLZLN67', {
+                  page_path: window.location.pathname,
+                });
+              `,
+              }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
