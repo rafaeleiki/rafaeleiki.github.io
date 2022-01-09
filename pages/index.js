84f6350e0369a792cc2613f6d109db6aa6adb221
Rafael Eiki <rafaeleikimi@gmail.com>
2022-01-09 01:04:10 -0300

    Fixed deploy for GitHub Pages

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import Link from 'next/link';
+import Link from '../components/link';
 
 import Footer from "../components/footer";
 import useTranslation from "../components/hooks/useTranslation";
