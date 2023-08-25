b1f9b4c1bfcbf6cc9139ed1f7a2144fd71114333
Rafael Eiki <rafaeleiki@users.noreply.github.com>
2023-08-24 21:28:47 -0300

    chore: removed unused code

--- pages/developer/index.js
+++ pages/developer/index.js
@@ -1,5 +1,3 @@
-import Breadcrumb from '../../components/breadcrumb';
-import Footer from '../../components/footer';
 import Grid from '../../components/grid';
 import Layout from '../../components/layout';
 
@@ -12,17 +10,16 @@ export default function DeveloperPage() {
 
   return (
     <Layout page="developer">
-      { <Grid
-          messagesPrefix="/developer"
-          initialGrid={EXPERIENCE_GRID}
-          key="experiences-section"
-        /> }
-
-      { <Grid
-          messagesPrefix="/developer"
-          initialGrid={EDUCATION_GRID}
-          key="education-section"
-        /> }
+      <Grid
+        messagesPrefix="/developer"
+        initialGrid={EXPERIENCE_GRID}
+        key="experiences-section"
+      />
+      <Grid
+        messagesPrefix="/developer"
+        initialGrid={EDUCATION_GRID}
+        key="education-section"
+      />
     </Layout>
   );
 }
